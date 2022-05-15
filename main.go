84c72927d3aac2e602b362ab0b2bba7ff81d28ce
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2022-05-15 09:27:42 +0530

    change the print line

--- main.go
+++ main.go
@@ -6,4 +6,7 @@ func main() {
 	fmt.Println("Welcome to our conference booking application")
 	fmt.Println("Get your tickets from here to attend")
 
+	var conferenceName = "Go Conference"
+
+	fmt.Println(conferenceName)
 }
