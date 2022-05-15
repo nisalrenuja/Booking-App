adfac54cee2f68cd4d45ce259e6035e8b6374aeb
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2022-05-15 10:27:04 +0530

    constants added

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome to our conference booking application")
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	const remainingTickets = 50
+
+	fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Println("We have", conferenceTickets, "conference tickets and remaining", remainingTickets, "conference tickets")
 	fmt.Println("Get your tickets from here to attend")
 
-	var conferenceName = "Go Conference"
-
-	fmt.Println(conferenceName)
 }
