2bae5ba7a5154244e830b9ef5af93b617865af50
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2022-05-16 20:11:12 +0530

    add calculation

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func main() {
 	conferenceName := "Go Conference"
-	const conferenceTickets int = 50
-	const remainingTickets int = 50
+	var conferenceTickets int = 50
+	var remainingTickets int = 50
 
 	fmt.Printf("\nWelcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have %v conference tickets and remaining %v conference tickets\n", conferenceTickets, remainingTickets)
@@ -28,6 +28,9 @@ func main() {
 	fmt.Println("\nEnter number of tickets:")
 	fmt.Scan(&userTickets)
 
+	remainingTickets = remainingTickets - userTickets
+
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will recive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
 }
