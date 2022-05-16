020e901715d95f46f049d0c12e922465a6f01a29
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2022-05-16 11:59:57 +0530

    Data types added

--- main.go
+++ main.go
@@ -3,12 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	const remainingTickets = 50
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	const remainingTickets int = 50
 
-	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("\nconferenceName is %T, conferenceTickets is %T and remainingTickets is %T\n", conferenceName, conferenceTickets, remainingTickets)
+	fmt.Printf("\nWelcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have %v conference tickets and remaining %v conference tickets\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets from here to attend")
+
+	var userName string
+	var userTickets int
+
+	userTickets = 2
+	userName = "Tom"
+
+	fmt.Printf("%v booked %v tikets\n", userName, userTickets)
 
 }
