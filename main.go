6e51e5b5f0529d4224553756119d50085857f55b
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2022-05-16 12:55:24 +0530

    removed unnecessary comments

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ func main() {
 	const conferenceTickets int = 50
 	const remainingTickets int = 50
 
-	//fmt.Printf("\nconferenceName is %T, conferenceTickets is %T and remainingTickets is %T\n", conferenceName, conferenceTickets, remainingTickets)
 	fmt.Printf("\nWelcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have %v conference tickets and remaining %v conference tickets\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets from here to attend")
