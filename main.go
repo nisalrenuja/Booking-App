e4370a5a55e5eab25ea92b6a9d9f88c92c4f1a8f
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2022-05-18 15:38:42 +0530

    slice added

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ func main() {
 	fmt.Printf("We have %v conference tickets and remaining %v conference tickets\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets from here to attend")
 
+	var bookings []string
+
 	var firstName string
 	var lastName string
 	var email string
@@ -29,8 +31,9 @@ func main() {
 	fmt.Scan(&userTickets)
 
 	remainingTickets = remainingTickets - userTickets
+	bookings = append(bookings, firstName+" "+lastName)
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will recive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
-
+	fmt.Printf("These are the all the bookings: %v\n", bookings)
 }
